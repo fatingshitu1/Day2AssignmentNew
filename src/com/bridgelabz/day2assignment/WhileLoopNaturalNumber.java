3a4eaadc5521477b15ae77bb465fb13929be65b2
shitalfating <shitalfating84@gmail.com>
2023-01-26 15:34:04 +0530

    Added:Calculting Natural Numbers

---
+++ src/com/bridgelabz/day2assignment/WhileLoopNaturalNumber.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.day2assignment;
+
+public class WhileLoopNaturalNumber {
+    public static void main(String[] args) {
+        int num = 5;
+        int sum = 0;
+        int i = 0;
+        while (i <= num){
+            System.out.println(sum);
+            i++;
+            sum +=i;
+        }
+    }
+}

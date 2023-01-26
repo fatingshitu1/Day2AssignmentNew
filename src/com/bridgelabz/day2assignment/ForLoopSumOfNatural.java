39829f7b369d0be9de71d707e4e8678b5a7d1fe4
shitalfating <shitalfating84@gmail.com>
2023-01-26 15:48:06 +0530

    Refactored: Natural Number by Using For Loop

---
+++ src/com/bridgelabz/day2assignment/ForLoopSumOfNatural.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.day2assignment;
+
+public class ForLoopSumOfNatural {
+    public static void main(String[] args) {
+        int num = 5;
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            System.out.println(sum);
+            sum += i;
+        }
+    }
+}
